89ea56baffe181da0af76f7339fa59b309f3e7a6
sschlosser7 <sschloss777@comcast.net>
2025-11-25 14:32:41 -0600

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,1 @@
-echo "print('Hello from Repo A')" > sample.py
-echo "print('Hello again')" >> sample.py
-echo "print('Hello again for the third time')" >> sample.py
-echo "print('Line 4')" >> sample.py
-echo "print('Line 5')" >> sample.py
-echo "print('Line 6')" >> sample.py
-echo "print('Line 7')" >> sample.py
+print("Hello from Repo A")
